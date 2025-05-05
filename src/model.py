ef7de09f88a98aedb969976b040512d7dc21c219
aiwitharkam <115545054+aiwitharkam@users.noreply.github.com>
2025-05-05 18:45:50 +0530

    Add linear regression model

---
+++ src/model.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, lr=0.01, epochs=1000):
+        self.lr = lr
+        self.epochs = epochs
+        self.w = None
+        self.b = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        for _ in range(self.epochs):
+            y_pred = np.dot(X, self.w) + self.b
+            dw = (1/n_samples) * np.dot(X.T, (y_pred - y))
+            db = (1/n_samples) * np.sum(y_pred - y)
+            self.w -= self.lr * dw
+            self.b -= self.lr * db
+
+    def predict(self, X):
+        return np.dot(X, self.w) + self.b
